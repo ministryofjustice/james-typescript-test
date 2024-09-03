8fb86dd342f37630614775f0da9b8ebfb4bfbb9b
james Hart <james.hart@digital.justice.gov.uk>
2024-09-03 16:42:38 +0100

    simplifying message

--- auditjson_to_sarif.py
+++ auditjson_to_sarif.py
@@ -54,7 +54,7 @@ def main():
     result_dict={
       'ruleID': this_result['name'],
       'level': this_result['severity'],
-      'message': {'text': json.dumps(this_result).replace(',',',\n')}
+      'message': {'text': 'This is some text'}
     }
     result_list.append(result_dict)
   output_dict['results']=result_list
